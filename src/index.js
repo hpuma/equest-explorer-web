187ae82937b2e6d8e50708736ef20bb1a1b45058
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-04-08 03:36:15 -0400

    Update index.js along with styling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import pages from "pages";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
+
+const { Dashboard, Homepage } = pages;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   
+    <Homepage />
+    <Dashboard />
   </React.StrictMode>
 );
 
