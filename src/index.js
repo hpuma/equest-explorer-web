d320f02281b62852e56b19c4611822740ddd7a58
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-04-12 02:26:58 -0400

    WIP: Implemented chart from antd

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const { Dashboard, Homepage } = pages;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Homepage />
     <Dashboard />
   </React.StrictMode>
 );
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
 import "./Dashboard.css";
-
+import { Table } from "antd";
+import EquestInstance from "api/equestserver";
 export default function Dashboard() {
+  let [ticker, setTicker] = useState("spy");
+  let [dataSource, setDataSource] = useState([
+    {
+      key: 1,
+      title: "title_val",
+      link: "link_val",
+      timestamp: "timestamp_val",
+      activity: "activity_val"
+    }
+  ]);
+  let [sortBy, setSortBy] = useState(null);
+
+  const columnNames = ["title", "link", "timestamp", "activity"];
+  const columns = columnNames.map((colName) => ({
+    title: colName.charAt(0).toUpperCase() + colName.slice(1),
+    dataIndex: colName,
+    key: colName
+  }));
+
+  useEffect(() => {
+    async function getData() {
+      const tickerData = await EquestInstance.getEverything(ticker, sortBy);
+      const { articles, count } = tickerData;
+
+      if (count !== 0) setDataSource(articles);
+    }
+    getData();
+  }, [ticker, sortBy]);
+
   return (
     <div id="dashboard-container">
       <h1>Dashboard</h1>
+      <Table dataSource={dataSource} columns={columns} />
     </div>
   );
 }
