d69157b6fa0ca28f6b47a3987f58919ef99d767b
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-04-27 16:14:31 -0400

    Moved API call to table component

--- src/components/TableWidget/TableWidget.jsx
+++ src/components/TableWidget/TableWidget.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import { Table } from "antd";
+import EquestInstance from "api/equestserver";
 import Config from "./utils/config";
 import * as defaults from "./utils/defaults";
 
 const { dataSourceDef } = defaults;
 
-export default function TableWidget({ dataSource = dataSourceDef }) {
+export default function TableWidget({ updateLoading, ticker, sortBy }) {
+  let [dataSource, setDataSource] = useState(dataSourceDef);
+
+  useEffect(() => {
+    // Make api request on ticker update
+    const getData = () => {
+      updateLoading(true, 0);
+      console.log("SEARCHED TICKER: ", ticker, "✅");
+      EquestInstance.getEverything(ticker, sortBy).then(({ articles, count }) => {
+        if (count) setDataSource(articles);
+        updateLoading(false, 215);
+      });
+    };
+
+    getData();
+  }, [ticker, sortBy]);
+
   return <Table dataSource={dataSource} columns={Config.getColumns()} />;
 }
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,19 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./Dashboard.css";
-
-import EquestInstance from "api/equestserver";
 import TableWidget from "components/TableWidget/TableWidget";
 import SearchWidget from "components/SearchWidget/SearchWidget";
 
 export default function Dashboard() {
   let [ticker, setTicker] = useState("AMZN");
-  let [dataSource, setDataSource] = useState();
   let [sortBy, setSortBy] = useState(null);
   let [loading, setLoading] = useState(false);
 
   // Search non empty search bars only
   const onSearch = (value) => {
-    if (value) setTicker(value);
+    if (value) setTicker(value.toUpperCase());
     else console.log("SEARCHED TICKER: EMPTY ❌");
   };
 
@@ -24,26 +21,11 @@ export default function Dashboard() {
     }, delay);
   };
 
-  useEffect(() => {
-    // Make api request on ticker update
-    const getData = () => {
-      updateLoading(true, 0);
-
-      console.log("SEARCHED TICKER: ", ticker, "✅");
-      const { articles, count } = EquestInstance.getEverything(ticker, sortBy).then((d) =>
-        updateLoading(false, 215)
-      );
-      if (count) setDataSource(articles);
-    };
-
-    getData();
-  }, [ticker, sortBy]);
-
   return (
     <div id="dashboard-container">
       <h1>Dashboard</h1>
-      <SearchWidget onSearch={onSearch} ticker={ticker} loading={loading} />
-      <TableWidget dataSource={dataSource} />
+      <SearchWidget ticker={ticker} loading={loading} onSearch={onSearch} />
+      <TableWidget ticker={ticker} sortBy={sortBy} updateLoading={updateLoading} />
     </div>
   );
 }
