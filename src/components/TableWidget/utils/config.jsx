54ab3a5334dd889caf326b203b21d8c8c98b6e7a
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-04-27 23:07:26 -0400

    Added sortable table and filters for author column

--- src/components/TableWidget/utils/config.jsx
+++ src/components/TableWidget/utils/config.jsx
@@ -1,10 +1,27 @@
 export default class Config {
   static getColumns() {
     const columnNames = ["title", "author", "description", "publishedAt"];
-    return columnNames.map((colName) => ({
+    const mappedColumns = columnNames.map((colName) => ({
       title: colName.charAt(0).toUpperCase() + colName.slice(1),
       dataIndex: colName,
-      key: colName
+      key: colName,
+      defaultSortOrder: "descend",
+      sorter: (a, b) => String(a).localeCompare(b.name)
     }));
+    mappedColumns[1] = {
+      ...mappedColumns[1],
+      filters: [
+        {
+          text: "investors.com",
+          value: "investors.com"
+        },
+        {
+          text: "benzinga.com",
+          value: "benzinga.com"
+        }
+      ],
+      onFilter: (value, record) => String(record.author).includes(value)
+    };
+    return mappedColumns;
   }
 }
