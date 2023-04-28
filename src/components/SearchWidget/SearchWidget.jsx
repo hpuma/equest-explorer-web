aaff69db512fdd69ae989cc1cce47f5f9578529f
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-04-28 00:36:13 -0400

    Added live search functionality for searchbar, limited to 5 requests per minute

--- src/components/SearchWidget/SearchWidget.jsx
+++ src/components/SearchWidget/SearchWidget.jsx
@@ -1,15 +1,51 @@
-import { Input } from "antd";
+import { AutoComplete, Input } from "antd";
+import { useState } from "react";
+import EquestInstance from "api/equestserver";
 const { Search } = Input;
+
 export default function SearchWidget({ onSearch, ticker, loading }) {
+  const [bestMatchesData, setBestMatches] = useState(null);
+  const [options, setOptions] = useState(null);
+  const tickerSearch = async (value) => {
+    if (value.length == 3) {
+      const { bestMatches } = await EquestInstance.getTickerSearch(value);
+      setBestMatches(bestMatches);
+      if (bestMatches) setOptions(searchResult());
+    }
+  };
+
+  const searchResult = () =>
+    bestMatchesData.map(({ symbol, name }) => ({
+      value: symbol,
+      label: (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between"
+          }}
+        >
+          <span>
+            <b>{symbol}</b>: {name}
+          </span>
+        </div>
+      )
+    }));
   return (
-    <Search
-      placeholder={ticker}
-      enterButton="Search"
-      size="large"
-      loading={loading}
-      onSearch={onSearch}
-      style={{ width: 400 }}
-      allowClear
-    />
+    <AutoComplete
+      dropdownMatchSelectWidth={252}
+      style={{ width: 300 }}
+      options={options}
+      onSearch={tickerSearch}
+    >
+      <Search
+        placeholder={ticker}
+        enterButton="Search"
+        size="large"
+        loading={loading}
+        onSearch={onSearch}
+        style={{ width: 400 }}
+        allowClear
+      />
+    </AutoComplete>
   );
 }
