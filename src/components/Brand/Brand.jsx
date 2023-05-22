63f9959e4589c2725aac3a077e5a31cf33663fe8
Henry Puma <pumahenry@live.com>
2023-05-22 04:47:32 -0400

    Created Brand component

---
+++ src/components/Brand/Brand.jsx
@@ -0,0 +1,8 @@
+import "./Brand.css";
+export default function Brand() {
+  return (
+    <div>
+      <img id="branding-logo" src={require("components/SearchWidget/images/SentiSys2.jpg")} />
+    </div>
+  );
+}
