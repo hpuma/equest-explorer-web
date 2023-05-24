23129893dc379f36fab1db66115544f212de8dcb
Henry Puma <pumahenry@live.com>
2023-05-24 03:39:26 -0400

    Updated nav bar to have dark mode switch

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,42 +2,45 @@ import React, { useState } from "react";
 
 import { HomeOutlined, DashboardOutlined, SettingOutlined, LoginOutlined } from "@ant-design/icons";
 
-import { Menu } from "antd";
-
-const items = [
-  {
-    label: "Home",
-    key: "home",
-    icon: <HomeOutlined />
-  },
-  {
-    label: "Dashboard",
-    key: "dashboard",
-    icon: <DashboardOutlined />
-  },
-  {
-    label: "Settings",
-    key: "settings",
-    icon: <SettingOutlined />,
-    children: [
-      {
-        label: "Log In",
-        key: "login",
-        icon: <LoginOutlined />
-      }
-    ]
-  }
-];
+import { Menu, Switch } from "antd";
 
 export default function NavBar() {
   const [current, setCurrent] = useState("mail");
+
+  const items = [
+    {
+      label: "Home",
+      key: "home",
+      icon: <HomeOutlined />
+    },
+    {
+      label: "Dashboard",
+      key: "dashboard",
+      icon: <DashboardOutlined />
+    },
+    {
+      label: "Settings",
+      key: "settings",
+      icon: <SettingOutlined />,
+      children: [
+        {
+          label: "Theme",
+          key: "theme",
+          icon: <Switch size="small" />
+        },
+        {
+          label: "Log In",
+          key: "login",
+          icon: <LoginOutlined />
+        }
+      ]
+    }
+  ];
 
   const onClick = (e) => {
     console.log("Click ✅", e.key);
     setCurrent(e.key);
   };
 
-  return (
-    <Menu onClick={onClick} selectedKeys={[current]} items={items} mode="horizontal" theme="dark" />
-  );
+  return <Menu onClick={onClick} selectedKeys={[current]} items={items} mode="horizontal" />;
 }
