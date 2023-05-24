00cc7abda081e6b4150be10843cc250b13462f24
Henry Puma <pumahenry@live.com>
2023-05-23 23:43:20 -0400

    Updated dashboard for search v2

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,66 +1,76 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Dashboard.css";
 import TickerWidget from "components/TickerWidget/TickerWidget";
 import SearchWidget from "components/SearchWidget/SearchWidget";
 import TableWidget from "components/TableWidget/TableWidget";
 import ChartWidget from "components/ChartWidget/ChartWidget";
+import Brand from "components/Brand/Brand";
 import NavBar from "components/NavBar/NavBar";
+import { Modal, Col, Row } from "antd";
 
-import { Col, Row } from "antd";
 export default function Dashboard() {
-  const BrandComponent = () => {
-    return (
-      <div>
-        <img
-          id="search-widget-equest-logo"
-          src={require("components/SearchWidget/images/SentiSys2.jpg")}
-        />
-      </div>
-    );
-  };
+  const [ticker, setTicker] = useState("AMZN");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const searchRef = useRef(null);
+  useEffect(() => {
+    const handleKeyDown = ({ keyCode }) => {
+      if (keyCode === 13) setIsModalOpen(false); // Enter Pressed
+      else if (keyCode >= 65 && keyCode <= 90) {
+        // Alphabetical is pressed
+        searchRef.current?.focus();
+        setIsModalOpen(true);
+      }
+    };
 
-  let [ticker, setTicker] = useState("AMZN");
-  let [loading, setLoading] = useState(false);
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   // Search non empty search bars only
   const onSearch = (value) => {
-    if (value) setTicker(value.toUpperCase());
-    else console.log("SEARCHED TICKER: EMPTY ❌");
+    if (typeof value === "string" && value.length > 0) {
+      setTicker(value);
+      setIsModalOpen(false);
+    } else if (!value) console.log("SEARCHED TICKER: EMPTY ❌");
   };
 
-  // Loading animation for widgets
-  const updateLoading = (isLoading, delay) => {
-    setTimeout(() => {
-      setLoading(isLoading);
-    }, delay);
-  };
+  // Spans
+  const tickerWidgetSpan = 8;
+  const emptyColSpan = tickerWidgetSpan + 4;
+  const navBarSpan = 24 - (tickerWidgetSpan + emptyColSpan);
 
   return (
     <div id="dashboard-container">
+      <Modal
+        title="Symbol Search"
+        open={isModalOpen}
+        onCancel={() => {
+          setIsModalOpen(false);
+        }}
+        footer={null}
+      >
+        <SearchWidget onSearch={onSearch} ticker={ticker} searchRef={searchRef} />
+      </Modal>
+
+      <Brand />
+      {/* Widgets */}
       <Row>
-        <Col span={2}>
-          <BrandComponent />
+        <Col span={tickerWidgetSpan}>
+          <TickerWidget ticker={ticker} />
         </Col>
-        <Col span={2}>TM</Col>
-        <Col span={8}>
-          <SearchWidget />
-        </Col>
-        <Col span={8}> </Col>
-        <Col span={4}>
+
+        <Col span={emptyColSpan}></Col>
+
+        <Col span={navBarSpan}>
           <NavBar />
         </Col>
       </Row>
-      <Row>
-        {/*Ticker */}
-        <Col span={4}>
-          <TickerWidget ticker={ticker} />
-        </Col>
-        {/*COL SPACING */}
-        <Col span={20}>SPACING</Col>
-      </Row>
 
-      <ChartWidget ticker={ticker} />
-      <TableWidget ticker={ticker} updateLoading={updateLoading} />
+      {/* <ChartWidget ticker={ticker} /> */}
+      <TableWidget ticker={ticker} />
     </div>
   );
 }
