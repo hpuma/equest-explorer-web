a5f741420dd04fa4f9265010a665b18078c8c988
Henry Puma <37905046+hpuma@users.noreply.github.com>
2024-01-25 18:26:05 -0500

    Add vite config file

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import babel from "vite-plugin-babel";
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({
+      jsx: "react"
+    })
+  ],
+  server: {
+    open: true,
+    port: 3001
+  }
+});
