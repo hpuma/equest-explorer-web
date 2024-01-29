a5a8ea3d030c82cb969bd9fdfa44b4063724e35c
Henry Puma <37905046+hpuma@users.noreply.github.com>
2024-01-28 21:13:18 -0500

    Added assets folder and updated image imports for brand

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,16 +1,12 @@
 import "./Brand.css";
 import React from "react";
+import Sentisys2 from "../../assets/Sentisys2.jpg";
+import Sentisys3 from "../../assets/Sentisys3.jpg";
+
 export default function Brand({ isDarkMode }) {
   return (
     <div>
-      <img
-        id="branding-logo"
-        src={
-          isDarkMode
-            ? "components/SearchWidget/images/Sentisys3.jpg"
-            : "components/SearchWidget/images/SentiSys2.jpg"
-        }
-      />
+      <img id="branding-logo" src={isDarkMode ? Sentisys3 : Sentisys2} />
     </div>
   );
 }
