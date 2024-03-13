3d2573562a27d5f2a6b45ff4d5aa8c60690d84d1
Henry Puma <pumahenry@live.com>
2024-03-13 00:17:51 -0400

    Imrpoved vite build config to sparse modules

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,17 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000
+  },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        }
+      }
+    }
   }
 });
